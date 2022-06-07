aa50bbffb25db9b05fef9f18b9b14949c0349eff
Jarno <wiemanboy@gmail.com>
2022-06-07 14:48:57 +0200

    heroku setup

--- src/main/java/JerseyConfig.java
+++ src/main/java/JerseyConfig.java
@@ -4,5 +4,5 @@ import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("restservices")
 public class JerseyConfig extends ResourceConfig {
-    public JerseyConfig() {packages("webservices");} // deze werkt niet
+    public JerseyConfig() {packages("webservices");}
 }
