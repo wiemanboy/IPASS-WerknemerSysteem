6b6e6cef7bf6dca97e1148379830c7e13de0bbfb
Jarno <wiemanboy@gmail.com>
2022-06-20 18:05:39 +0200

    redirect after login

--- src/main/webapp/src/service/authenticationService.js
+++ src/main/webapp/src/service/authenticationService.js
@@ -1,20 +1,20 @@
 export default class AuthenticationService {
-    constructor() {
-        this.baseUrl = "https://ipass-werknemersysteem-jarno.herokuapp.com/restservices/authenticate"
-    }
+  constructor() {
+      this.baseUrl = "https://ipass-werknemersysteem-jarno.herokuapp.com/restservices/authenticate"
+  }
 
-    login(data) {               
-        const requestOptions = {
-          method: 'POST',
-          mode: "same-origin",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(data),
-          redirect: 'follow'
-        };
-        
-        return fetch("https://ipass-werknemersysteem-jarno.herokuapp.com/restservices/authenticate", requestOptions)
-          .then(response => {if (response.ok) {return response.json()} else return "wrong username or password"})
-          .then(result => window.sessionStorage.setItem("JWTtoken", result.JWT))
-          .catch(error => console.log('error', error));
-    }
+  login(data) {               
+      const requestOptions = {
+        method: 'POST',
+        mode: "same-origin",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data),
+        redirect: 'follow'
+      };
+      
+      return fetch("https://ipass-werknemersysteem-jarno.herokuapp.com/restservices/authenticate", requestOptions)
+        .then(response => {if (response.ok) {window.location.assign('./pages/tablePage.html'); return response.json();} else throw "wrong username or password"})
+        .then(result => window.sessionStorage.setItem("JWTtoken", result.JWT))
+        .catch(error => console.log('error', error));
+  }
 }
