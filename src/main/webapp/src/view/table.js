757cb321092be6126ae780106a428231c3d94678
Jarno <wiemanboy@gmail.com>
2022-06-21 16:25:06 +0200

    change row color

--- src/main/webapp/src/view/table.js
+++ src/main/webapp/src/view/table.js
@@ -19,21 +19,26 @@ function renderKlussen() {
 function renderWerknemers() {}
 
 function addTableRow(klus) {
+    // get template data
     const rowNode = document.querySelector("#tableTemplate").content.cloneNode(true);
     const tableData = rowNode.querySelectorAll("td");
 
+    // get table
     const table = document.querySelector("tbody");
-    rowNode.querySelector('tr').setAttribute('id', klus.id);
 
+    // set id
+    rowNode.querySelector('tr').setAttribute('id', klus.id);
     if (klus.id % 2 == 0) {rowNode.querySelector('tr').setAttribute('class', "even");}
 
+    const begindatum = "" + klus.begindatum;
+
+    // set table data
     tableData[0].textContent = klus.klant;
     tableData[1].textContent = klus.adres;
-    tableData[2].textContent = klus.begindatum;
+    tableData[2].textContent = begindatum;
 
+    // add table data
     table.appendChild(rowNode);
-
-    console.log(table);
 }
 
 // ---------- Main Program ---------- //
